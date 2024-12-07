69e3b16424c3567e0abd6083e22efa2438383055
piotrvish <97231296+piotrvish@users.noreply.github.com>
2024-12-07 11:42:00 +0100

    Minor tweaks

--- js/image-enlarge.js
+++ js/image-enlarge.js
@@ -5,27 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     images.forEach(img => {
         img.addEventListener("click", () => {
-            if (overlay.style.visibility === "visible") {
+            if (overlay.classList.contains("active")) {
                 // Close the enlarged view
-                overlay.style.visibility = "hidden";
-                overlay.style.opacity = "0";
+                overlay.classList.remove("active");
                 pageContent.classList.remove("blurred");
                 overlay.innerHTML = "";
             } else {
                 // Open the enlarged view
-                overlay.style.visibility = "visible";
-                overlay.style.opacity = "1";
+                overlay.classList.add("active");
                 pageContent.classList.add("blurred");
                 overlay.innerHTML = `<img id="image-overlay" src="${img.src}" alt="${img.alt}">`;
                 document.getElementById("image-overlay").style.display = "block";
+                document.getElementById("image-overlay").style.opacity = "1";
             }
         });
     });
 
     // Close on Overlay Click
     overlay.addEventListener("click", () => {
-        overlay.style.visibility = "hidden";
-        overlay.style.opacity = "0";
+        overlay.classList.remove("active");
         pageContent.classList.remove("blurred");
         overlay.innerHTML = "";
     });
