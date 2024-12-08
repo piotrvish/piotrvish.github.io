c83ce862c024e7b778bb9af6fdfaee93dd3e3cb3
piotrvish <97231296+piotrvish@users.noreply.github.com>
2024-12-08 20:31:54 +0100

    Add files via upload

---
+++ js/scroll-animation.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.1 });
+
+    images.forEach(image => observer.observe(image));
+});
