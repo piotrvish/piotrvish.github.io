3f3fb8089d7144ba4e7ca1123baf43ed0b072b28
piotrvish <97231296+piotrvish@users.noreply.github.com>
2024-12-08 20:34:33 +0100

    Delete js directory

--- js/avoid-orphans-text.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("p").forEach(paragraph => {
-    let words = paragraph.innerHTML.trim().split(" ");
-    if (words.length > 1) {
-        words[words.length - 2] += `&nbsp;${words.pop()}`;
-        paragraph.innerHTML = words.join(" ");
-    }
-});
